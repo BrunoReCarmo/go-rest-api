b6f3e1a36303c1fa49a60dc164b3e1db88b3c577
brunorecarmo <bruno.reis.carmo@gmail.com>
2025-12-31 13:51:52 -0300

    Check server route

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	// Server is using the Gin package
+	server := gin.Default()
+
+	// The endpoint will be "/hello"
+	// Simple inline function, gin.H maps the response message
+	// The expected response is:
+	
+	server.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "world",
+		})
+	})
+
+	//localhost:4000
+	server.Run(":4000")
 }
